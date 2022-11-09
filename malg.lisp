b0ef854ba0018b89fdd149671e9a7c4fbb23761d
Sergey Rukavishnikov <sergeyruk312@gmail.com>
2022-11-09 21:42:13 +0300

    The comp-foo argument in insertion-sort is now optional

--- malg.lisp
+++ malg.lisp
@@ -1,8 +1,9 @@
 (in-package :malg)
 
-(defun insertion-sort (A comp)
+(defun insertion-sort (A &optional comp-foo)
+  (if (not comp-foo) (setf comp-foo #'>))
   (defun  while (i key)
-    (if (and (>= i 0) (funcall comp (aref A i) key))
+    (if (and (>= i 0) (funcall comp-foo (aref A i) key))
         (progn (setf (aref A (+ i 1)) (aref A i))
                (while (- i 1) key)) (+ i 1)))
   (loop for j from 1 to (- (length A) 1) do
@@ -10,3 +11,4 @@
             ((key (aref A j))
              (i (- j 1)))
           (setf (aref A (while i key)) key))))
+
